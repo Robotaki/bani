2f96184ab61b1306c1d405491f3931a6d7144aae
Vladislav Buturlya <sibtime2014@gmail.com>
2023-11-12 14:12:11 +0300

    block change slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,6 +1,22 @@
-$(document).ready(function() {
-  $('.slider').slick({
+$(document).ready(function () {
+  $(".slider").slick({
     arrows: true,
     slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+  var slideCount = $('.slider').slick('getSlick').slideCount;
+
+  for (var i = 0; i < slideCount; i++) {
+    $('.pagination').append('<button>' + (i + 1) + '</button>');
+  }
+
+  $('.pagination button').click(function(){
+    var index = $(this).index();
+    $('.slider').slick('slickGoTo', index);
+  });
+
+  $('.slider').on('afterChange', function(event, slick, currentSlide){
+    $('.pagination button').removeClass('active');
+    $('.pagination button').eq(currentSlide).addClass('active');
   });
 });
