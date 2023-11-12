87162007050c5a5c51885046da57adc204e9d785
Vladislav Buturlya <sibtime2014@gmail.com>
2023-11-12 09:30:15 +0300

    added adaptive

--- dist/js/scroll__up.js
+++ dist/js/scroll__up.js
@@ -1,10 +1,21 @@
-window.addEventListener('scroll', function() {
-    var scroll = document.querySelector('.upward');
-    scroll.classList.toggle("active", this.window.scrollY>500)
+window.addEventListener("scroll", function () {
+  var scroll = document.querySelector(".upward");
+  scroll.classList.toggle("active", this.window.scrollY > 500);
 });
-    function scrollTopTop() {
-        window.scrollTo({
-            top:0,
-            behavior: 'smooth'
-        })
-    }
+function scrollTopTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+window.addEventListener("scroll", function () {
+  var scroll = document.querySelector(".vidgets");
+  scroll.classList.toggle("active", this.window.scrollY > 700);
+});
+function scrollTopTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
