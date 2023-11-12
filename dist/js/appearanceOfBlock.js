742d461d7aceff8d1fe8e0508153b8bba0f6aa56
Vladislav Buturlya <sibtime2014@gmail.com>
2023-11-12 19:13:40 +0300

    adding an adaptive block

--- dist/js/appearanceOfBlock.js
+++ dist/js/appearanceOfBlock.js
@@ -1,15 +1,26 @@
 const toggleButton = document.getElementById('toggleButton');
 const optionsBlocks = document.querySelectorAll('.options__block');
-let currentIndex = 0;
+//let currentIndex = 0;
+
+// toggleButton.addEventListener('click', () => {
+//     if (currentIndex < optionsBlocks.length) {
+//         optionsBlocks[currentIndex].style.display = 'block';
+//         currentIndex++
+//     } else {
+//         optionsBlocks.forEach(block => {
+//             block.style.display = 'none';
+//         });
+//         currentIndex = 0;
+//     }
+// })
 
 toggleButton.addEventListener('click', () => {
-    if (currentIndex < optionsBlocks.length) {
-        optionsBlocks[currentIndex].style.display = 'block';
-        currentIndex++
-    } else {
-        optionsBlocks.forEach(block => {
-            block.style.display = 'none';
-        });
-        currentIndex = 0;
-    }
-})
+    // Переключаем видимость блоков по клику на кнопку
+    optionsBlocks.forEach(block => {
+      if (block.style.display === 'none') {
+        block.style.display = 'block';
+      } else {
+        block.style.display = 'none';
+      }
+    });
+  });
