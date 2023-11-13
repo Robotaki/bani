0cd1a0a8c8dbffcd8550114dde82f258f4a03157
Vladislav Buturlya <sibtime2014@gmail.com>
2023-11-13 12:56:08 +0300

    slider settings adaptive

--- dist/js/reviews-slider.js
+++ dist/js/reviews-slider.js
@@ -1,7 +1,28 @@
-$(document).ready(function() {
-    $('.slider-reviews').slick({
-      arrows: false,
-      variableWidth: true,
-      slidesToShow: 2,
-    });
+$(document).ready(function () {
+  $(".slider-reviews").slick({
+    arrows: false,
+    variableWidth: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 2500,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 6,
+          infinite: true,
+          arrows: true,
+        }
+      },
+      {
+        breakpoint: 1920,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          arrows: false,
+        }
+      },
+    ]
   });
+});
